14a2bc6602c7aeb462eb978582a774ecd091e2e5
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-25 16:49:24 -0400

    Primer commit

---
+++ src/main/java/com/example/cruduniquecategoryback/CrudUniqueCategoryBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cruduniquecategoryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUniqueCategoryBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrudUniqueCategoryBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/cruduniquecategoryback/CrudUniqueCategoryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruduniquecategoryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUniqueCategoryBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
